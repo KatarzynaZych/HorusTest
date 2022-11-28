9743132e2e558f834a3a340dc912004b1bdf25a2
unknown <korzonek.katarzyna@gmail.com>
2022-11-28 11:19:46 +0100

    second

--- src/main/java/Wall.java
+++ src/main/java/Wall.java
@@ -1,27 +1,36 @@
 import java.util.List;
 import java.util.Optional;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Wall implements Structure {
 
-    private CompositeBlock compositeBlock;
-    private List<Block> blocks = compositeBlock.getBlocks();
+    private final List<Block> blocks;
+
+    public Wall(CompositeBlock blocks) {
+        this.blocks = blocks.getBlocks();
+    }
 
     @Override
     public Optional<Block> findBlockByColor(String color) {
         featureValidation(color);
-        return blocks.stream().filter(b -> (color.equals(b.getColor()))).findAny();
+        return getBlockStream((i) -> color.equals(i.getColor())).findAny();
     }
 
     @Override
     public List<Block> findBlocksByMaterial(String material) {
         featureValidation(material);
-        return blocks.stream().filter(b -> (material.equals(b.getMaterial()))).collect(Collectors.toList());
+        return getBlockStream((i) -> material.equals(i.getMaterial())).collect(Collectors.toList());
     }
 
     @Override
     public int count() {
         return blocks == null ? 0 : blocks.size();
+    }
+
+    private Stream<Block> getBlockStream(Predicate<Block> hasFeature) {
+        return blocks.stream().filter(hasFeature);
     }
 
     private static void featureValidation(String feature) {
@@ -30,3 +39,4 @@ public class Wall implements Structure {
         }
     }
 }
+
